80df8a8914fe48b67ee55daa8b8494df3e817f01
rajkharvar <rajkharvar1808@gmail.com>
2022-05-11 13:18:36 +0530

    Minor changes to homepage

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,21 +1,27 @@
 import React from "react";
 import clsx from "clsx";
 import styles from "./HomepageFeatures.module.css";
-import { SiReadthedocs } from "react-icons/si";
 import { RiBookReadLine } from "react-icons/ri";
 import { MdLaptopMac } from "react-icons/md";
+import { FaRocket } from "react-icons/fa";
 import Link from "@docusaurus/Link";
 
 const FeatureList = [
   {
     title: "Getting started",
-    Svg: <SiReadthedocs size={40} color="#40429B" />,
+    Svg: <FaRocket size={40} />,
     description: <>Learn how you can get started with Razor Network.</>,
     linkURL: "/docs/what-is-an-oracle",
   },
   {
+    title: "Whitepaper",
+    Svg: <RiBookReadLine size={40} />,
+    description: <>Read about Razor Network Whitepaper</>,
+    linkURL: "/docs/whitepaper/intro",
+  },
+  {
     title: "Running Razor Node",
-    Svg: <MdLaptopMac size={40} color="#40429B" />,
+    Svg: <MdLaptopMac size={40} />,
     description: (
       <>
         Running Razor node allows you to participate in network and earn block
@@ -24,17 +30,11 @@ const FeatureList = [
     ),
     linkURL: "/docs/razor-go/installation",
   },
-  {
-    title: "Whitepaper",
-    Svg: <RiBookReadLine size={40} color="#40429B" />,
-    description: <>Read about Razor Network Whitepaper</>,
-    linkURL: "/docs/whitepaper/intro",
-  },
 ];
 
 function Feature({ Svg, title, description, linkURL }) {
   return (
-    <Link className={clsx("col col--4")} to={linkURL}>
+    <Link className={clsx(`col col--4 ${styles.link}`)} to={linkURL}>
       <div className="text--center">{Svg}</div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
